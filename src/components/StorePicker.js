b7834e27bcd57637965039e8a417dcea67b23454
Septira Taruna <stftaruna@Septiras-MacBook-Pro.local>
2018-07-18 15:44:18 +0700

    lesson 6

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+
 
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello!</p>
+        return (
+            <form className="store-selector">
+                <h2> Please Enter Store Name</h2>
+                <input type="text" required placeholder="Store Name"/>
+                <button type="submit">Visit Store >></button>
+            </form>
+        )
     }
 }
 
 export default StorePicker;
+
+/* By default, react JSX render() method can only return ONE parent element. 
+                    If want to have multiple adjacent elements, use React.Fragment:
+                    <React.Fragment>
+                        <p>one</p>
+                        <div>
+                            <p>two</p>
+                        </div>
+                    </React.Fragment>
+                    This rule also applies to commenting, 
+                    DO NOT comment outside element as it will give out errors.
+                    Only comment inside the element */
