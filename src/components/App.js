44e16316005dc7563e136c3c13f50fcfd6a233b2
Septira Taruna <stftaruna@Septiras-MacBook-Pro.local>
2018-07-19 13:41:44 +0700

    lesson 9

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "./Header";
-import Order from "./Order";
-import Inventory from "./Inventory";
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
 
 class App extends React.Component{
     render() {
         return(
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="Fishermen's Favorite" rank={1} best={true}/>
                 </div>
                 <Order />
                 <Inventory />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
-import React from "react";
+import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
+const Header = ({tagline, rank}) => (
             <header className="top">
                 <h1>Catch 
                     <span className="ofThe">
@@ -12,12 +10,29 @@ class Header extends React.Component {
                     Day
                 </h1>
                 <h3 className="tagline">
-                <span>Fresh Daily</span>
+                <span>#{rank} {tagline}</span>
             </h3>
             </header>
+)
 
-        )
-    }
-}
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>Catch 
+//                     <span className="ofThe">
+//                         <span className="of">Of</span>
+//                         <span className="the">The</span>
+//                     </span>
+//                     Day
+//                 </h1>
+//                 <h3 className="tagline">
+//                 <span>{this.props.tagline}</span>
+//             </h3>
+//             </header>
+
+//         )
+//     }
+// }
 
 export default Header;
